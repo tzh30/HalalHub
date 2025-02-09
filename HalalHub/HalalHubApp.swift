07efd22000fef4870b58a5314f5726443b9484f7
Thant Zin Htoon <mushyyy@Mac.attlocal.net>
2025-02-09 10:18:23 -0600

    Initial commit: First version of Xcode project

---
+++ HalalHub/HalalHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalalHubApp.swift
+//  HalalHub
+//
+//  Created by Thant Zin Htoon on 9/2/25.
+//
+
+import SwiftUI
+
+@main
+struct HalalHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HalalHubTests/HalalHubTests.swift
@@ -0,0 +1,17 @@
+//
+//  HalalHubTests.swift
+//  HalalHubTests
+//
+//  Created by Thant Zin Htoon on 9/2/25.
+//
+
+import Testing
+@testable import HalalHub
+
+struct HalalHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
